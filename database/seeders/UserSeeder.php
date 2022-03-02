994d6000d8dc391bc168bd8fe4455b0f2ffc5e45
NicMMuir <nicholas.muir2797@gmail.com>
2022-03-02 21:35:07 +0200

    Added alerts

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,10 +17,17 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+            'name' => 'Ricus',
+            'email' => 'Ricus'.'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        DB::table('users')->insert([
             'name' => 'Nic',
             'email' => 'Nic'.'@gmail.com',
             'password' => Hash::make('password'),
         ]);
+
         DB::table('users')->insert([
             'name' => Str::random(10),
             'email' => Str::random(10).'@gmail.com',
